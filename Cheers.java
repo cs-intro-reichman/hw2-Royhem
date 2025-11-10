ed676a4934ae27d3341fe633cf8e0a14bbeb800c
Royhem <roy.hemelniker@post.runi.ac.il>
2025-11-10 10:15:09 +0200

    Update Cheers.java

--- Cheers.java
+++ Cheers.java
@@ -8,6 +8,23 @@ Great work boaz!
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+
+                String first = args[0];
+ 		int num = Integer.parseInt(args[1]);
+                int count = first.length();
+
+                for(int i = 0;i<count;i++)       {
+                     System.out.println("Give me an "+ first.charAt(i) + ": "+ first.charAt(i)+"!");
+
+                }  
+                        
+                for(int z = 0;z<num;z++)       {
+
+                     System.out.println(first+"!!!");
+
+
+                }
+
+
         }
 }
