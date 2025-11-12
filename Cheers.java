43ec9f4b97654fa01ea0c67f78a76ee4bfcc1c7d
Royhem <roy.hemelniker@post.runi.ac.il>
2025-11-12 12:07:44 +0200

    Update Cheers.java

--- Cheers.java
+++ Cheers.java
@@ -22,7 +22,7 @@ public class Cheers {
                                 if (word.charAt(j)==first.charAt(i)) 
                                 {
                                         System.out.println("Give me an "+ first.charAt(i) + ": "+ first.charAt(i)+"!");  
-                                         //System.out.println("Give me a  "+ first.charAt(i) + ": "+ first.charAt(i)+"!");  
+                                         System.out.println("Give me a  "+ first.charAt(i) + ": "+ first.charAt(i)+"!");  
                                         // System.out.println("Give me a  "+ first.charAt(i) + ": "+ first.charAt(i)+"!");    
  
                                 }
@@ -46,3 +46,4 @@ public class Cheers {
 
         }
 }
+
